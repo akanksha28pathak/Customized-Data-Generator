8ccd12ea90f8f20bd7354062ce527f400c92ce8d
akanksha28pathak <akanksha28.iitkgp@gmail.com>
2021-02-25 12:15:45 -0600

    Update label_data_formation.py

--- label_data_formation.py
+++ label_data_formation.py
@@ -3,13 +3,13 @@ import numpy as np
 num_abnormal=100
 labels1 = {}
 for j in range(1,num_abnormal+1):
- id_j=str(j)+’_AB’
+ id_j=str(j)+'_AB’
  labels1[id_j] = 1
  del num_abnormal
 num_normal=100
 labels2={}
 for j in range(1,num_normal+1):
-  id_j=str(j)+’_NOR’
+  id_j=str(j)+'_NOR’
   labels2[id_j] =0
 # merge two dictionary
 labels2.update(labels1)
